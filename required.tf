6ed346d1e17252726548a391f91d06ebb225b292
Herbert <herbertenorio@gmail.com>
2024-06-22 17:48:02 -0300

    feat: adicionando teste para backend

--- required.tf
+++ required.tf
@@ -6,7 +6,15 @@ terraform {
     }
   }
 
+#   backend "azurerm" {
+#     resource_group_name  = "tfstate"
+#     storage_account_name = "<storage_account_name>"
+#     container_name       = "tfstate"
+#     key                  = "terraform.tfstate"
+#   }
+
   required_version = ">= 1.0"
+
 }
 
 provider "azurerm" {
