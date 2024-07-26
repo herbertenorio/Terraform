383f062992b379c83001d7c86383750a5cabd781
Herbert de Gusmão Tenório <herbert.de.g.tenorio@avanade.com>
2024-07-26 15:45:29 -0300

    feat: refact pr check- 001

--- main.tf
+++ main.tf
@@ -3,13 +3,13 @@ resource "azurerm_resource_group" "rg" {
   location = var.location
 }
 
-# resource "azurerm_storage_account" "example" {
-#   name                     = var.storage_account_name
-#   resource_group_name      = azurerm_resource_group.rg.name
-#   location                 = azurerm_resource_group.rg.location
-#   account_tier             = "Standard"
-#   account_replication_type = "LRS"
+resource "azurerm_storage_account" "example" {
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
 
-#   enable_https_traffic_only = false
+  enable_https_traffic_only = false
 
-# } ---0
+}
